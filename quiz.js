9fba05498043ca07d8cb1046db47ef9f7a1a4233
Khan <S1942405@online.houstonisd.org>
2021-06-02 19:21:38 -0500

    first commit

---
+++ quiz.js
@@ -0,0 +1,84 @@
+let buttonOne = document.getElementById('button-c-1')
+let buttonTwo = document.getElementById('button-c-2')
+let buttonThree = document.getElementById('button-c-3')
+let buttonFour = document.getElementById('button-c-4')
+let buttonFive = document.getElementById('button-c-5')
+let buttonSix = document.getElementById('button-c-6')
+let buttonSeven = document.getElementById('button-c-7')
+let buttonEight = document.getElementById('button-c-8')
+let buttonNine = document.getElementById('button-c-9')
+let buttonTen = document.getElementById('button-c-10')
+let buttonEleven = document.getElementById('button-c-11')
+let buttonTwelve = document.getElementById('button-c-12')
+let buttonThirteen = document.getElementById('button-c-13')
+let buttonFourteen = document.getElementById('button-c-14')
+let buttonFifteen = document.getElementById('button-c-15')
+let buttonSixten = document.getElementById('button-c-16')
+
+buttonOne.addEventListener('click',() =>{
+    buttonOne.style.backgroundColor = 'red'
+})
+
+buttonTwo.addEventListener('click',() =>{
+    buttonTwo.style.backgroundColor = 'green'
+})
+  
+buttonThree.addEventListener('click',() =>{
+    buttonThree.style.backgroundColor = 'red'
+})
+
+buttonFour.addEventListener('click',() =>{
+    buttonFour.style.backgroundColor = 'red'
+})
+
+buttonFive.addEventListener('click',() =>{
+    buttonFive.style.backgroundColor = 'red'
+})
+      
+
+buttonSix.addEventListener('click',() =>{
+    buttonSix.style.backgroundColor = 'red'
+})
+  
+buttonSeven.addEventListener('click',() =>{
+    buttonSeven.style.backgroundColor = 'red'
+})
+
+
+buttonEight.addEventListener('click',() =>{
+    buttonEight.style.backgroundColor = 'green'
+})
+      
+   
+buttonNine.addEventListener('click',() =>{
+    buttonNine.style.backgroundColor = 'red'
+})
+      
+buttonTen.addEventListener('click',() =>{
+    buttonTen.style.backgroundColor = 'red'
+})
+
+buttonEleven.addEventListener('click',() =>{
+    buttonEleven.style.backgroundColor = 'green'
+})
+
+buttonTwelve.addEventListener('click',() =>{
+    buttonTwelve.style.backgroundColor = 'red'
+})
+      
+buttonThirteen.addEventListener('click',() =>{
+    buttonThirteen.style.backgroundColor = 'green'
+})
+
+buttonFourteen.addEventListener('click',() =>{
+    buttonFourteen.style.backgroundColor = 'red'
+})
+       
+buttonFifteen.addEventListener('click',() =>{
+    buttonFifteen.style.backgroundColor = 'red'
+})
+
+buttonSixten.addEventListener('click',() =>{
+    buttonSixten.style.backgroundColor = 'red'
+})
+         
